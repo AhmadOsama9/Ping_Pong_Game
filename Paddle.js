26d8e92be7414923291ae939f0caec4a6c866c66
AhmedOsama9 <ahmedosamaa975@gmail.com>
2023-04-11 00:08:42 +0200

    The whole game

---
+++ Paddle.js
@@ -0,0 +1,33 @@
+const SPEED = .02;
+
+export default class Paddle{
+    constructor(paddleElem){
+        this.paddleElem = paddleElem;
+        this.reset();
+    }
+
+    get position(){
+        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue("--position"));
+    }
+
+    set position(value){
+        this.paddleElem.style.setProperty("--position", value);
+    }
+
+    reset(){
+        this.position = 50;
+    }
+
+    rect(){
+        return this.paddleElem.getBoundingClientRect();
+    }
+
+    update(delta, ballHeight){
+        /*this.position = ballHeight;
+          Doing that will make it impossible
+          to win against the computer
+        */
+       this.position += SPEED * delta * (ballHeight - this.position);
+       
+    }
+}
